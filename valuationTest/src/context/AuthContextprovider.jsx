9b9f61fb240e1a5ef6ab4d6759dc5e362f75e97a
S K <skaltamash10.99@gmail.com>
2024-06-02 18:08:24 +0530

    evaluation

--- valuationTest/src/context/AuthContextprovider.jsx
+++ valuationTest/src/context/AuthContextprovider.jsx
@@ -2,10 +2,15 @@ import { createContext, useState } from "react";
 
 let AuthContext = createContext();
 
-export function AuthContextprovider = ({ children }) => {
+export function AuthContextprovider  ({ children }){
   let [auth, setAuth] = useState({
     token: null,
     isloggedin: false,
   });
   return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
 };
+
+
+export function AuthContextprovider ({children}){
+  
+}
